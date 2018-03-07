5b675df252593f88a53ab531680fbe6f79afade0
mendozaju <jmendoza@despegar.com>
2018-03-07 11:44:41 -0300

    Se agrega healtcheck

--- src/main/java/com/blue/wappsender/api/controller/ApiController.java
+++ src/main/java/com/blue/wappsender/api/controller/ApiController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.blue.wappsender.api.controller.healt.HealtResponse;
+
 @Controller
 public class ApiController {
 	
@@ -12,6 +14,16 @@ public class ApiController {
 	@ResponseBody
 	public String getStatus() {
 		return "OK";
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, path="/", produces="application/json; charset=utf-8")
+	@ResponseBody
+	public HealtResponse healtCheck() {
+		HealtResponse response = new HealtResponse();
+		response.setStatus("OK");
+		response.setVersion("0.0.1-SNAPSHOT");
+		response.setDescription("API para el envio de mensajes");
+		return response;
 	}
 
 }
---
+++ src/main/java/com/blue/wappsender/api/controller/healt/HealtResponse.java
@@ -0,0 +1,36 @@
+package com.blue.wappsender.api.controller.healt;
+
+public class HealtResponse {
+	
+	private String status;
+	private String version;
+	private String description;
+	
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	
+	
+
+}
